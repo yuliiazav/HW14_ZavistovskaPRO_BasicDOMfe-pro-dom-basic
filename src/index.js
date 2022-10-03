9c11c9787cfaee762732e757eb68902deff10467
Your Nameyuliiazav <didiky1993@gmail.com>
2022-10-03 17:48:13 +0300

    added HW files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let cards = document.querySelectorAll('.item');
+    for (let item = 0; item < cards.length; item++) {
+      if (item % 2 === 0) {
+             cards[item].style.backgroundColor = 'red';
+           }
+        }
+   };
 
-export const findElement = () => {};
+
+export const findElement = () => {
+    let cards = document.querySelectorAll('li');
+//let card = .classList.contains("class")
+    for (let item = 0; item < cards.length; item++) {
+        if (cards[item].classList.contains("likedItem")) {
+            cards[item].style.backgroundColor = 'blue';
+        }
+    }
+};
